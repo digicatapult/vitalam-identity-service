499b7f00cc1e01ff0cf0fd5d30894505f0301508
Jonathan Gray <jonmattgray@gmail.com>
2022-04-11 15:30:27 +0100

    use types package

--- app/util/appUtil.js
+++ app/util/appUtil.js
@@ -1,84 +1,14 @@
 const { ApiPromise, WsProvider } = require('@polkadot/api')
+const types = require('@digicatapult/dscp-node')
 
-const {
-  API_HOST,
-  API_PORT,
-  METADATA_KEY_LENGTH,
-  METADATA_VALUE_LITERAL_LENGTH,
-  PROCESS_IDENTIFIER_LENGTH,
-} = require('../env')
+const { API_HOST, API_PORT } = require('../env')
 const logger = require('../logger')
 const { getMemberAliasesDb } = require('../db')
 
 const provider = new WsProvider(`ws://${API_HOST}:${API_PORT}`)
 const apiOptions = {
   provider,
-  types: {
-    Address: 'MultiAddress',
-    LookupSource: 'MultiAddress',
-    PeerId: 'Vec<u8>',
-    Key: 'Vec<u8>',
-    TokenId: 'u128',
-    RoleKey: 'Role',
-    TokenMetadataKey: `[u8; ${METADATA_KEY_LENGTH}]`,
-    TokenMetadataValue: 'MetadataValue',
-    Token: {
-      id: 'TokenId',
-      original_id: 'TokenId',
-      roles: 'BTreeMap<RoleKey, AccountId>',
-      creator: 'AccountId',
-      created_at: 'BlockNumber',
-      destroyed_at: 'Option<BlockNumber>',
-      metadata: 'BTreeMap<TokenMetadataKey, TokenMetadataValue>',
-      parents: 'Vec<TokenId>',
-      children: 'Option<Vec<TokenId>>',
-    },
-    ProcessIO: {
-      roles: 'BTreeMap<RoleKey, AccountId>',
-      metadata: 'BTreeMap<TokenMetadataKey, TokenMetadataValue>',
-      parent_index: 'Option<u32>',
-    },
-    MetadataValue: {
-      _enum: {
-        File: 'Hash',
-        Literal: `[u8; ${METADATA_VALUE_LITERAL_LENGTH}]`,
-        TokenId: 'TokenId',
-        None: null,
-      },
-    },
-    Role: {
-      _enum: ['Owner', 'Customer', 'AdditiveManufacturer', 'Laboratory', 'Buyer', 'Supplier', 'Reviewer'],
-    },
-    ProcessIdentifier: `[u8; ${PROCESS_IDENTIFIER_LENGTH}]`,
-    ProcessVersion: 'u32',
-    ProcessId: {
-      id: 'ProcessIdentifier',
-      version: 'ProcessVersion',
-    },
-    Process: {
-      status: 'ProcessStatus',
-      restrictions: 'Vec<Restriction>',
-    },
-    ProcessStatus: {
-      _enum: ['Disabled', 'Enabled'],
-    },
-    Restriction: {
-      _enum: {
-        None: '()',
-        SenderOwnsAllInputs: '()',
-        FixedNumberOfInputs: 'FixedNumberOfInputsRestriction',
-        FixedNumberOfOutputs: 'FixedNumberOfOutputsRestriction',
-      },
-    },
-    FixedNumberOfInputsRestriction: {
-      num_inputs: 'u32',
-    },
-    FixedNumberOfOutputsRestriction: {
-      num_outputs: 'u32',
-    },
-    IsNew: 'bool',
-    Restrictions: 'Vec<Restriction>',
-  },
+  types,
 }
 
 const api = new ApiPromise(apiOptions)
