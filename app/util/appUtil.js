a1947ca8398d5ea0ed8ecdc6c8314ddec9b7d093
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-07-12 14:10:23 +0100

    Remove unnecessary env vars

--- app/util/appUtil.js
+++ app/util/appUtil.js
@@ -1,12 +1,6 @@
 const { buildApi } = require('@digicatapult/dscp-node')
 
-const {
-  API_HOST,
-  API_PORT,
-  METADATA_KEY_LENGTH,
-  METADATA_VALUE_LITERAL_LENGTH,
-  PROCESS_IDENTIFIER_LENGTH,
-} = require('../env')
+const { API_HOST, API_PORT } = require('../env')
 const logger = require('../logger')
 const { getMemberAliasesDb } = require('../db')
 
@@ -14,9 +8,6 @@ const { api } = buildApi({
   options: {
     apiHost: API_HOST,
     apiPort: API_PORT,
-    metadataKeyLength: METADATA_KEY_LENGTH,
-    metadataValueLiteralLength: METADATA_VALUE_LITERAL_LENGTH,
-    processorIdentifierLength: PROCESS_IDENTIFIER_LENGTH,
   },
 })
 
